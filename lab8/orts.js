c483d9f9c4921c0adcbcaa84762dada480ec8b44
nur <bolataidana73@gmail.com>
2024-11-14 18:39:01 +0800

    lab

--- lab8/orts.js
+++ lab8/orts.js
@@ -4,9 +4,13 @@ let davhar = 9;
 let orts = 3;
 let ail = 4;
 toot -= 1;
+if(toot>108){
 var N = toot / (davhar * ail) + 1;
 document.writeln(parseInt(N) + "-r orts");
 var M = (toot % (davhar * ail)) / ail + 1;
 document.writeln(parseInt(M) + "-r davhar");
 var L = (toot % ail) + 1;
 document.writeln(parseInt(L) + "-r haalga");
+}
+else
+{document.writeln("toot oldsongui")}
