c163b6a21931e305b4172136dce89ab4ec5d8b22
nur <bolataidana73@gmail.com>
2024-12-19 18:02:30 +0800

    quiz nemsen

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -19,7 +19,7 @@ async function loadQuestions(category) {
 
 
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
+  for (let i = array.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
